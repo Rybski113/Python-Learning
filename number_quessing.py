5b5db559aad0c72358faa2f11017c8fe15b0b444
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-27 13:06:07 +0200

    Update number_quessing.py

--- number_quessing.py
+++ number_quessing.py
@@ -1,3 +1,30 @@
 import random 
 
-random.randrange(start, stop)
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger than 0 next time. ')
+        quit()
+else:
+    print('Please type a number next')  
+    quit()      
+
+random_number = random.randint(1, top_of_range)
+
+while True:
+    user_quess = input("make a quess: ")
+    if user_quess.isdigit():
+       user_quess = int(user_quess)
+
+    
+    else:
+       print('Please type a number next')  
+    continue
+
+    if user_quess == random_number:
+        print("You got it!")
+    else:
+        print("You got it wrong!")    
